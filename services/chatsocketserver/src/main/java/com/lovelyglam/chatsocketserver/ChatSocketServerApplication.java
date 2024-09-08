b61f03c273598b0f9f69982af35033f41ddbf6bc
AnataAria <kakata28419@gmail.com>
2024-09-08 08:19:06 +0700

    Add docker file

---
+++ services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/ChatSocketServerApplication.java
@@ -0,0 +1,11 @@
+package com.lovelyglam.chatsocketserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatSocketServerApplication {
+    public static void main (String [] args) {
+        SpringApplication.run(ChatSocketServerApplication.class, args);
+    }
+}
---
+++ services/systemserver/src/main/java/com/lovelyglam/systemserver/config/SystemServerApplicationConfig.java
@@ -0,0 +1,8 @@
+package com.lovelyglam.systemserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SystemServerApplicationConfig {
+    
+}
