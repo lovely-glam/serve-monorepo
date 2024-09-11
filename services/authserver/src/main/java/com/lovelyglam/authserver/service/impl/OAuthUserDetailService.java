c315f595c6b2fcb465ddf83455b3b9b97ec17f22
AnataAria <kakata28419@gmail.com>
2024-09-11 23:13:25 +0700

    Fix config and add utils package

--- services/authserver/src/main/java/com/lovelyglam/authserver/service/impl/OAuthUserDetailService.java
+++ services/authserver/src/main/java/com/lovelyglam/authserver/service/impl/OAuthUserDetailService.java
@@ -2,6 +2,7 @@ package com.lovelyglam.authserver.service.impl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -13,8 +14,10 @@ import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import com.lovelyglam.database.model.constant.LoginMethodType;
 import com.lovelyglam.database.model.entity.LoginMethod;
 import com.lovelyglam.database.repository.LoginMethodRepository;
+import com.lovelyglam.utils.general.EnumUtils;
 
 import lombok.RequiredArgsConstructor;
 @Service
@@ -29,8 +32,7 @@ public class OAuthUserDetailService implements OAuth2UserService<OAuth2UserReque
 
         String oauth2Id = oAuth2User.getName();
         String oauth2Provider = userRequest.getClientRegistration().getRegistrationId();
-
-        LoginMethod user = loginMethodRepository.findLoginMethodByExternalIdAndLoginMethodType(oauth2Id, oauth2Provider.toUpperCase()).orElseThrow();
+        LoginMethod user = loginMethodRepository.findLoginMethodByExternalIdAndLoginMethodType(oauth2Id,EnumUtils.convertStringToEnum(LoginMethodType.class, oauth2Provider)).orElseThrow();
         return new DefaultOAuth2User(
                 rolesToAuthority(user),
                 oAuth2User.getAttributes(),
