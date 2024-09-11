91b4ded8b6d37393a3616979d6b9aeb5f99e9158
AnataAria <kakata28419@gmail.com>
2024-09-11 22:23:02 +0700

    Add base entity

--- packages/database/src/main/java/com/lovelyglam/database/model/entity/ServiceBooking.java
+++ packages/database/src/main/java/com/lovelyglam/database/model/entity/ServiceBooking.java
@@ -3,6 +3,6 @@ package com.lovelyglam.database.model.entity;
 import jakarta.persistence.Entity;
 
 @Entity(name = "service_booking")
-public class ServiceBooking {
+public class ServiceBooking extends BaseEntity {
     
 }
