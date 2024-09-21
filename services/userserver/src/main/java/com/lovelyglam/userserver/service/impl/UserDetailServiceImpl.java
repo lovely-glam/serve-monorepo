75e7966a95a672b0c6cbc5a42a8a1a6dcbf43ffd
AnataAria <kakata28419@gmail.com>
2024-09-21 19:44:41 +0700

    Add User Detail

--- services/userserver/src/main/java/com/lovelyglam/userserver/service/impl/UserDetailServiceImpl.java
+++ services/userserver/src/main/java/com/lovelyglam/userserver/service/impl/UserDetailServiceImpl.java
@@ -1,15 +1,35 @@
 package com.lovelyglam.userserver.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.lovelyglam.database.model.entity.UserAccount;
+import com.lovelyglam.database.repository.UserAccountRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class UserDetailServiceImpl implements UserDetailsService {
 
+    private final UserAccountRepository userAccountRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'loadUserByUsername'");
+        UserAccount userAccount = userAccountRepository.findUserAccountByUsername(username).orElseThrow();
+        return new User(userAccount.getUsername(),userAccount.getHashPassword(),rolesToAuthority(userAccount));
+    }
+    private Collection<GrantedAuthority> rolesToAuthority(UserAccount user) {
+        var roleList = new ArrayList<GrantedAuthority>();
+        roleList.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return roleList;
     }
     
 }
