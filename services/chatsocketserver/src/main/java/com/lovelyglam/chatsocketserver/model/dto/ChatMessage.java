66fab7aa0ab2abf63ce6e59e48ceb23615c211d4
AnataAria <kakata28419@gmail.com>
2024-10-05 17:32:40 +0700

    Edit change for socket and register business

--- services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/model/dto/ChatMessage.java
+++ services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/model/dto/ChatMessage.java
@@ -4,14 +4,17 @@ import java.time.LocalDateTime;
 
 import com.lovelyglam.chatsocketserver.model.constant.MessageStatus;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ChatMessage {
     private String senderName;
-    private String recieveName;
     private String message;
     private LocalDateTime time;
     private MessageStatus status;
