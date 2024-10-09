87fa392aa4955867d050c0bde7cf922a44de91a2
haiha0402 <nguyenha24bmt@gmail.com>
2024-10-09 11:43:25 +0700

    ha/fix merge

--- services/authserver/src/main/java/com/lovelyglam/authserver/service/AuthService.java
+++ services/authserver/src/main/java/com/lovelyglam/authserver/service/AuthService.java
@@ -1,11 +1,10 @@
 package com.lovelyglam.authserver.service;
 
 import com.lovelyglam.database.model.dto.request.LocalAuthenticationRequest;
-import com.lovelyglam.database.model.dto.request.OAuth2AuthenticationRequest;
 import com.lovelyglam.database.model.dto.response.AuthenticationResponse;
 
 public interface AuthService {
     AuthenticationResponse localAuthentication (LocalAuthenticationRequest request);
-    AuthenticationResponse oauthAuthentication (OAuth2AuthenticationRequest request);
+    AuthenticationResponse oauthAuthentication ();
     AuthenticationResponse businessAuthentication (LocalAuthenticationRequest request);
 }
