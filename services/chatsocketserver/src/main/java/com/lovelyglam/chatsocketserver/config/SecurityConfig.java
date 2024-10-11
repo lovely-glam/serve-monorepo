ec9d059a96b282ffd66aea5d8e1d4edfd6857d78
AnataAria <kakata28419@gmail.com>
2024-10-11 20:29:30 +0700

    Config base security for chat socket

--- services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/config/SecurityConfig.java
+++ services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/config/SecurityConfig.java
@@ -1,11 +1,24 @@
 package com.lovelyglam.chatsocketserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.socket.EnableWebSocketSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @EnableWebSocketSecurity
 public class SecurityConfig {
+    @Bean
+    SecurityFilterChain authenticationFitterChain (HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests((auth) -> 
+                auth.anyRequest().permitAll())
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
 }
