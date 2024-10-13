4c6f28d9b0a47950b7758397a429f4a739a3c3b3
AnataAria <kakata28419@gmail.com>
2024-10-13 13:46:51 +0700

    config login for system admin

--- services/authserver/src/main/java/com/lovelyglam/authserver/service/impl/SystemUserDetailService.java
+++ services/authserver/src/main/java/com/lovelyglam/authserver/service/impl/SystemUserDetailService.java
@@ -18,12 +18,20 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class SystemUserDetailServiceImpl implements UserDetailsService {
+public class SystemUserDetailService implements UserDetailsService {
     private final SystemAccountRepository systemAccountRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         var result = systemAccountRepository.findSystemAccountByUsername(username).orElseThrow(() -> new UsernameNotFoundException("Not found account with this username"));
-        return new User(result.getUsername(), result.getHashPassword(), rolesToAuthority(result));
+        return new User(
+            result.getUsername(), 
+            result.getHashPassword(), 
+            result.isActive(),
+            true,
+            true,
+            true,
+            rolesToAuthority(result)
+        );
     }
     private Collection<GrantedAuthority> rolesToAuthority(SystemAccount user) {
         var roleList = new ArrayList<GrantedAuthority>();
