5c59278aea904cd599e3d8f1958971e0e64ec759
AnataAria <kakata28419@gmail.com>
2024-10-17 02:03:20 +0700

    Adjust socket

--- services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/controller/ChatController.java
+++ services/chatsocketserver/src/main/java/com/lovelyglam/chatsocketserver/controller/ChatController.java
@@ -10,7 +10,8 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
-import com.lovelyglam.chatsocketserver.model.dto.ChatMessageDto;
+import com.lovelyglam.chatsocketserver.model.dto.ChatMessageRequest;
+import com.lovelyglam.chatsocketserver.model.dto.ChatMessageResponse;
 import com.lovelyglam.chatsocketserver.service.ChatService;
 
 
@@ -23,15 +24,15 @@ public class ChatController {
     private final SimpMessagingTemplate simpMessagingTemplate;
     @MessageMapping("/chat/{roomId}")
     @SendTo("/topic/messages/{roomId}")
-    public ChatMessageDto send (@Payload ChatMessageDto message, @DestinationVariable(value = "roomId") BigDecimal roomId ) {
+    public ChatMessageResponse send (@Payload ChatMessageRequest message, @DestinationVariable(value = "roomId") BigDecimal roomId ) {
         return chatService.storeMessage(message, roomId);
     }
 
     @MessageMapping("/chat-histories/{roomId}")
     public void getMessageHistory(@DestinationVariable(value = "roomId") BigDecimal roomId) {
-        Collection<ChatMessageDto> message = chatService.getAllMessageFromRoomId(roomId);
+        Collection<ChatMessageResponse> message = chatService.getAllMessageFromRoomId(roomId);
         message.forEach(entity -> {
-            simpMessagingTemplate.convertAndSend(String.format("/topic/messages/%d", roomId.intValue()), message);
+            simpMessagingTemplate.convertAndSend(String.format("/topic/messages/%d", roomId.intValue()), entity);
         });
     }
 }
