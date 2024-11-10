5f3ebafaad336c2073aa47fbffe6febc3ad95451
AnataAria <kakata28419@gmail.com>
2024-11-10 16:53:42 +0700

    remove security path on contact api

--- services/userserver/src/main/java/com/lovelyglam/userserver/config/SecurityConfig.java
+++ services/userserver/src/main/java/com/lovelyglam/userserver/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig {
                 exceptionHandling.authenticationEntryPoint(glamAuthenticationEntryPoint);
             })
             .authorizeHttpRequests((auth) -> 
-                auth.requestMatchers("/auth/**", "/pings/**", "/api-docs/**", "/swagger-ui/**", "/profiles/**", "/nail-services/**", "/feedbacks/**", "/payments/booking/callback/**").permitAll().anyRequest().authenticated())
+                auth.requestMatchers("/auth/**", "/pings/**", "/api-docs/**", "/swagger-ui/**", "/profiles/**", "/nail-services/**", "/feedbacks/**", "/payments/booking/callback/**", "/contacts/**").permitAll().anyRequest().authenticated())
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .cors(cors -> cors.configurationSource(corsConfig.corsConfigurationSource()));
         http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
